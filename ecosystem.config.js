8624961ab54d994ecfd76378f485abe5032a794d
anshumanpate <snyp2001@gmail.com>
2023-09-02 17:17:48 +0530

    second changes ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "vt-backend-pw2",
-      script: "script/rails",
+      script: "bin/rails",
       args: "server -b 0.0.0.0 -p 3000",
       interpreter: "bundle",
     },
